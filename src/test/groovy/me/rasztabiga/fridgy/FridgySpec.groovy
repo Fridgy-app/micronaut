b713040e3ed40789c97ccc8b88e0db53d8d51aed
Bartłomiej Rasztabiga <contact@rasztabiga.me>
2021-09-18 23:49:09 +0200

    exclude integration tests

--- src/test/groovy/me/rasztabiga/fridgy/FridgySpec.groovy
+++ src/test/groovy/me/rasztabiga/fridgy/FridgySpec.groovy
@@ -1,19 +1,12 @@
 package me.rasztabiga.fridgy
 
-import io.micronaut.runtime.EmbeddedApplication
-import io.micronaut.test.extensions.spock.annotation.MicronautTest
 import spock.lang.Specification
-import jakarta.inject.Inject
 
-@MicronautTest
 class FridgySpec extends Specification {
 
-    @Inject
-    EmbeddedApplication<?> application
-
-    void 'test it works'() {
+    void 'test ?'() {
         expect:
-        application.running
+        true
     }
 
 }
