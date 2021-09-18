f3dbfb5fa50c7b99b491982f87a0f20001e94505
Bartłomiej Rasztabiga <contact@rasztabiga.me>
2021-09-19 00:47:44 +0200

    leave TODO

--- product-catalog/src/main/kotlin/me/rasztabiga/fridgy/productcatalog/ConferenceController.kt
+++ product-catalog/src/main/kotlin/me/rasztabiga/fridgy/productcatalog/ConferenceController.kt
@@ -4,6 +4,7 @@ import io.micronaut.http.annotation.Controller
 import io.micronaut.http.annotation.Get
 
 @Controller("/conferences")
+// # TODO: secure with keycloak, https://micronaut-projects.github.io/micronaut-security/latest/guide/#jwks
 class ConferenceController(private val conferenceService: ConferenceService) {
 
     @Get("/random")
